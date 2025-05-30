c16e4e0566aadcff4c3dab8d467f292d8091a294
sydhds <sylvain@status.im>
2025-05-30 16:12:35 +0200

    Add missing file: proof_generation.rs

---
+++ prover/src/proof_generation.rs
@@ -0,0 +1,33 @@
+use alloy::primitives::Address;
+use rln_proof::{RlnIdentifier, RlnUserIdentity};
+use std::sync::Arc;
+
+pub(crate) struct ProofGenerationData {
+    pub(crate) user_identity: RlnUserIdentity,
+    pub(crate) rln_identifier: Arc<RlnIdentifier>,
+    pub(crate) tx_counter: u64,
+    pub(crate) tx_sender: Address,
+    pub(crate) tx_hash: Vec<u8>,
+}
+
+impl From<(RlnUserIdentity, Arc<RlnIdentifier>, u64, Address, Vec<u8>)> for ProofGenerationData {
+    /// Create a new ProofGenerationData - assume tx_hash is 32 bytes long
+    fn from(
+        (user_identity, rln_identifier, tx_counter, tx_sender, tx_hash): (
+            RlnUserIdentity,
+            Arc<RlnIdentifier>,
+            u64,
+            Address,
+            Vec<u8>,
+        ),
+    ) -> Self {
+        debug_assert!(tx_hash.len() == 32);
+        Self {
+            user_identity,
+            rln_identifier,
+            tx_counter,
+            tx_sender,
+            tx_hash,
+        }
+    }
+}
