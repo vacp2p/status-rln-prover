aab6dd74a8615ba49096026c2c5f7d220aa3b8b3
sydhds <sylvain@status.im>
2025-07-15 12:05:02 +0200

    Initial code for tracing + oltp

--- prover_cli/src/main.rs
+++ prover_cli/src/main.rs
@@ -8,20 +8,36 @@ use tracing::{
     // error,
     // info
 };
-use tracing_subscriber::{EnvFilter, fmt, layer::SubscriberExt, util::SubscriberInitExt};
+use tracing_subscriber::{EnvFilter, layer::SubscriberExt, util::SubscriberInitExt};
+
+use opentelemetry::trace::TracerProvider;
+use opentelemetry_otlp::WithTonicConfig;
 // internal
 use prover::{AppArgs, AppArgsConfig, metrics::init_metrics, run_prover};
 
 #[tokio::main]
 async fn main() -> Result<(), Box<dyn std::error::Error + Send + Sync + 'static>> {
-    // debug!("Args: {:?}", std::env::args());
+    // tracing
 
     let filter = EnvFilter::builder()
         .with_default_directive(LevelFilter::INFO.into())
+        // Use a more compact, abbreviated log format
         .from_env_lossy();
+
+    let fmt_layer = tracing_subscriber::fmt::layer();
+
+    let telemetry_layer = match create_otlp_tracer_provider() {
+        Some(tracer_provider) => {
+            let tracer = tracer_provider.tracer("prover-cli");
+            Some(tracing_opentelemetry::OpenTelemetryLayer::new(tracer))
+        }
+        None => None,
+    };
+
     tracing_subscriber::registry()
-        .with(fmt::layer())
+        .with(fmt_layer)
         .with(filter)
+        .with(telemetry_layer)
         .init();
 
     // let app_args = AppArgs::parse();
@@ -59,4 +75,40 @@ async fn main() -> Result<(), Box<dyn std::error::Error + Send + Sync + 'static>
     init_metrics(app_args.metrics_ip, &app_args.metrics_port);
 
     run_prover(app_args).await
+}
+
+fn create_otlp_tracer_provider() -> Option<opentelemetry_sdk::trace::SdkTracerProvider> {
+    if !std::env::vars().any(|(name, _)| name.starts_with("OPENTELEMETRY_")) {
+        return None;
+    }
+    let protocol =
+        std::env::var("OPENTELEMETRY_EXPORTER_OTLP_PROTOCOL").unwrap_or("grpc".to_string());
+
+    let exporter = match protocol.as_str() {
+        "grpc" => {
+            let mut exporter = opentelemetry_otlp::SpanExporter::builder().with_tonic();
+
+            // Check if we need TLS
+            if let Ok(endpoint) = std::env::var("OPENTELEMETRY_EXPORTER_OTLP_ENDPOINT") {
+                if endpoint.starts_with("https") {
+                    exporter = exporter.with_tls_config(
+                        opentelemetry_otlp::tonic_types::transport::ClientTlsConfig::default()
+                            .with_enabled_roots(),
+                    );
+                }
+            }
+            exporter.build().expect("Failed to create tonic exporter")
+        }
+        "http/protobuf" => opentelemetry_otlp::SpanExporter::builder()
+            .with_http()
+            .build()
+            .expect("Failed to create http/protobuf exporter"),
+        p => panic!("Unsupported protocol {}", p),
+    };
+
+    Some(
+        opentelemetry_sdk::trace::SdkTracerProvider::builder()
+            .with_batch_exporter(exporter)
+            .build(),
+    )
 }
