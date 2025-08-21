2e86c528b8d36afc7de4d56949cb074cf5dcfe10
sydhds <sylvain@status.im>
2025-08-21 12:27:40 +0200

    Add env var for prover bench

--- prover/benches/prover_bench.rs
+++ prover/benches/prover_bench.rs
@@ -113,6 +113,14 @@ fn proof_generation_bench(c: &mut Criterion) {
 
     let rayon_num_threads = std::env::var("RAYON_NUM_THREADS")
         .unwrap_or("".to_string());
+    let proof_service_count_default = 4;
+    let proof_service_count = std::env::var("PROOF_SERVICE_COUNT")
+        .map(|c| u16::from_str(c.as_str()).unwrap_or(proof_service_count_default))
+        .unwrap_or(proof_service_count_default);
+    let proof_count_default = 5;
+    let proof_count = std::env::var("PROOF_COUNT")
+        .map(|c| u32::from_str(c.as_str()).unwrap_or(proof_count_default))
+        .unwrap_or(proof_count_default);
 
     let rt = tokio::runtime::Builder::new_current_thread()
         .enable_all()
@@ -122,7 +130,7 @@ fn proof_generation_bench(c: &mut Criterion) {
     let port = 50051;
     let temp_folder = tempfile::tempdir().unwrap();
     let temp_folder_tree = tempfile::tempdir().unwrap();
-    let proof_service_count = 4;
+    // let proof_service_count = 4;
     let app_args = AppArgs {
         ip: IpAddr::V4(Ipv4Addr::new(127, 0, 0, 1)),
         port,
@@ -177,10 +185,11 @@ fn proof_generation_bench(c: &mut Criterion) {
     let mut group = c.benchmark_group("prover_bench");
     // group.sampling_mode(criterion::SamplingMode::Flat);
 
-    let proof_count = 5;
+    // let proof_count = 5;
+    let proof_count = proof_count as usize;
 
     group.throughput(Throughput::Elements(proof_count as u64));
-    let benchmark_name = format!("proof_generation_proof_{}_proof_service_{}_rt_{}", proof_count, proof_service_count, rayon_num_threads);
+    let benchmark_name = format!("prover_proof_{}_proof_service_{}_rt_{}", proof_count, proof_service_count, rayon_num_threads);
     group.bench_with_input(
         BenchmarkId::new(benchmark_name, proof_count),
         &proof_count,
